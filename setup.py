4627d3a7d96fcea79042dff3b49279d634702982
kuldeep13022005 <kuldeeppurbiya13@gmail.com>
2025-12-13 21:51:30 +0530

    Logging and Exception

--- setup.py
+++ setup.py
@@ -1,26 +1,55 @@
+# ## building our application as a package itself
+
+
+# from setuptools import find_packages,setup
+# from typing import List
+
+
+# HYPEN_E_DOT="-e ."
+# def get_requirements(file_path:str)->List[str]:
+#     '''
+#     this function will return the list of requirements
+#     '''
+#     requirements=[]
+#     with open(file_path) as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements=[req.replace("\n","") for req in requirements]
+
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
+#     return requirements        
+
+# ## this is called the metadata
+
+# setup(    
+#     name='mlproject',
+#     version='0.0.1',
+#     author = 'Krish',
+#     author_email='krishnaik06@gmail.com',
+#     packages=find_packages(),
+#     install_requires=get_requirements('requirements.txt')
+# )
+
 from setuptools import find_packages,setup
-from typing import List
 
-
-HYPEN_E_DOT="-e ."
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
+def get_requirements(file_path:str) ->List[str]:
+    '''this function will give the list of the librariries to be installed from the requirements.txt file'''
     requirements=[]
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        file_obj.readlines()
+        fil
+    
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements        
 
-setup(    
+setup(
+    
     name='mlproject',
     version='0.0.1',
-    author = 'Krish',
+    author='Krish',
     author_email='krishnaik06@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
+
+    
 )
+
