8f73d8c909e9ccf40c3bdb15da69dff6fe2d1ed0
kuldeep13022005 <kuldeeppurbiya13@gmail.com>
2025-12-15 03:28:40 +0530

    EDA and Problem Statement

--- setup.py
+++ setup.py
@@ -31,13 +31,24 @@
 # )
 
 from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
 
 def get_requirements(file_path:str) ->List[str]:
-    '''this function will give the list of the librariries to be installed from the requirements.txt file'''
+    '''
+    this function will give the list of the librariries to be installed from the requirements.txt file
+    '''
+    
     requirements=[]
     with open(file_path) as file_obj:
-        file_obj.readlines()
-        fil
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    
+        if HYPHEN_E_DOT in requirements :
+            requirements.remove(HYPHEN_E_DOT) 
+    
+    return requirements
     
 
 
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() ## the exc_tb variable tells which file on which line the errro has occured 
